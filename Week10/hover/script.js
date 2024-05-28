d712a76d7229844c957926d46dcd4a610652ac12
NICALOD3ON <129828101+NICALOD3ON@users.noreply.github.com>
2024-05-28 15:55:03 +1000

    week 10 work

---
+++ Week10/hover/script.js
@@ -0,0 +1,20 @@
+const catImg = document.querySelector("#cat-img");
+console.log(catImg)
+
+const moreInfo = document.querySelector("#more-info");
+console.log(moreInfo)
+
+catImg.addEventListener("mouseover", showInfo);
+catImg.addEventListener("mouseout", hideInfo);
+
+moreInfo.innerHTML = "<p>Add to Cart </p>";
+
+function showInfo()
+{
+    moreInfo.classList.add("show");
+}
+
+function hideInfo()
+{
+    moreInfo.classList.remove("show");
+}
---
+++ Week10/navigate/script.js
@@ -0,0 +1,8 @@
+const footerButton = document.querySelector("#footer-button");
+console.log(footerButton);
+
+footerButton.addEventListener("click", gotoFooter);
+
+function gotoFooter () {
+    window.location.href = //www.coolmathgames.com/0-fireboy-and-water-girl-in-the-forest-temple
+}
